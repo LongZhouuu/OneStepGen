84bbdde6d5d6bd8044c8f20616df2f81cd5e0518
ZhouLong <zlon0008@student.monash.edu>
2026-04-04 20:29:19 +1100

    Setup boostrap and primevue frame

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import './assets/main.css'
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// PrimeVue
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura' 
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset:Aura
+  }
+} )
+app.mount('#app')
