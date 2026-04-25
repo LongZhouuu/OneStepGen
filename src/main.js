4f1bfe85ddf5a5ec84824ed038460f45cbc395dd
ZhouLong <zlon0008@student.monash.edu>
2026-04-25 20:41:13 +1000

    Adjust Homepage layout

--- src/main.js
+++ src/main.js
@@ -4,19 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './assets/fonts.css'
 
-// PrimeVue
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura' 
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-    preset:Aura
-  }
-} )
 app.mount('#app')
